7af98762f85df31de00f5174c87d22c358b91691
Viren Negi <meetme2meat@gmail.com>
2014-02-22 11:54:07 +0530

    The Following commit ensure - Allow users to set and use specific customs mapper and layout options for the sessions controller

--- lib/padrino/warden.rb
+++ lib/padrino/warden.rb
@@ -21,17 +21,21 @@ module Padrino
       app.set :auth_error_message,     "You have provided invalid credentials."
       app.set :auth_success_message,   "You have logged in successfully."
       app.set :deauth_success_message, "You have logged out successfully."
+      # Custom map options and layout for the sessions controller
       app.set :auth_login_template,    'sessions/login'
-
+      app.set :auth_login_path,  'sessions/login' unless app.respond_to?(:auth_login_path)
+      app.set :auth_unauthenticated_path,'/unauthenticated' unless app.respond_to?(:auth_unauthenticated_path)
+      app.set :auth_logout_path,'sessions/logout' unless app.respond_to?(:auth_logout_path)
+      app.set :auth_login_layout, true
       # OAuth Specific Settings
       app.set :auth_use_oauth, false
+      app.set :default_strategies, [:password] unless app.respond_to?(:default_strategies)
 
       app.set :warden_failure_app, app
       app.set :warden_default_scope, :session
       app.set(:warden_config) { |manager| nil }
-
       app.use ::Warden::Manager do |manager|
-        manager.scope_defaults :session, strategies: [:password]
+        manager.scope_defaults :session, strategies: app.default_strategies
         manager.default_scope = app.warden_default_scope
         manager.failure_app   = app.warden_failure_app
         app.warden_config manager
